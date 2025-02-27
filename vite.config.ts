6899fb19a64c99e7ddc8e87824a1e4039a040b95
Emile301 <2351795@csfoy.ca>
2025-02-27 15:59:43 -0500

    Création projet

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+base: '/appweb-trpr01/'
+export default defineConfig({
+  plugins: [vue()]
+})
